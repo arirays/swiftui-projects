e56a333d6ace2625d8f9eb6635d2277b5f97370b
arirays <arishhu@gmail.com>
2023-12-03 14:50:17 -1000

    remote push notification sample app

---
+++ RemotePushApp/RemotePushApp/RemotePushAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  RemotePushAppApp.swift
+//  RemotePushApp
+//
+//  Created by AS on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RemotePushAppApp: App {
+    
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
