b92711128e46065be07c45931572055a7a182729
arirays <arishhu@gmail.com>
2024-01-16 12:33:42 -1000

    Added networking request exercise

---
+++ NetworkingRequest.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+import Foundation
+
+let menuAddress = ""
+
+let menuURL = URL(string: menuAddress)
+guard let menuURL = menuURL else {
+    throw NSError()
+}
+
+let request = URLRequest(url: menuURL)
+let task = URLSession.shared.dataTask(with: menuURL)
+{ data, response, error in
+    if let data = data {
+        let responseString = String(data: data, encoding: .utf8)
+        print(responseString ?? "https://www.google.com")
+    }
+}
+
+task.resume()
