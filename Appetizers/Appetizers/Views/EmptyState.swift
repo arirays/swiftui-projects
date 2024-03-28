f0e1579cc60167afed416e4fecaf7b6145f4c3ca
arirays <arishhu@gmail.com>
2024-03-27 14:22:41 -1000

    Added EmptyState view

---
+++ Appetizers/Appetizers/Views/EmptyState.swift
@@ -0,0 +1,39 @@
+//
+//  EmptyState.swift
+//  Appetizers
+//
+//  Created by AS on 3/25/24.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    
+    let imageName: String
+    let message: String
+    
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .ignoresSafeArea(edges: .all)
+            VStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 150)
+                Text(message)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .padding()
+            }
+        }
+    }
+}
+
+
+
+#Preview {
+    EmptyState(imageName: "empty-order", message: "This is our test message. \nI'm making it a little long for testing")
+}
--- Appetizers/Appetizers/Views/OrderView.swift
+++ Appetizers/Appetizers/Views/OrderView.swift
@@ -8,11 +8,45 @@
 import SwiftUI
 
 struct OrderView: View {
+    
+    @State private var orderItems = MockData.orderItems
+    
     var body: some View {
         NavigationStack {
-            Text("Order View")
-                .navigationTitle("🧾Orders")
+            ZStack {
+                VStack {
+                    List {
+                        ForEach(orderItems) { appetizer in
+                            AppetizerRowView(appetizer: appetizer)
+                        }
+                        .onDelete(perform: deleteItems)
+                    }
+                    .listStyle(PlainListStyle())
+                    
+                    Button {
+                        print("Order placed")
+                    } label: {
+                        Text("$99.00 - Place order ")
+                            .font(.title3)
+                            .fontWeight(.semibold)
+                            .frame(width: 260, height: 50)
+                            .foregroundStyle(.white)
+                            .background(Color(.brandPrimary))
+                            .clipShape(.rect(cornerRadius: 10))
+                    }
+                    .padding(.bottom, 30)
+                }
+                
+                if orderItems.isEmpty {
+                    EmptyState(imageName: "empty-order", message: "You have no items in your order. Please add an appetizer!")
+                }
+            }
+            .navigationTitle("🧾Orders")
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 }
 
